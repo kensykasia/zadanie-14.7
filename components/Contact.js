d70ce4311a0808185d7fcc76719dda4925a6027c
Katarzyna Kensy <zbierek@gmail.com>
2018-08-08 15:31:20 +0200

    beautify all .js files

--- components/Contact.js
+++ components/Contact.js
@@ -1,20 +1,43 @@
 var Contact = React.createClass({
-  propTypes: {
-    item: React.PropTypes.object.isRequired,
-  },
+    propTypes: {
+        item: React.PropTypes.object.isRequired,
+    },
 
-  render: function() {
-    return (
-      <div className = {'contactItem'}>
-        <img
-          className= {'contactImage'}
-          src = {'./img/user.png'}/>
+    render: function() {
+        return ( <
+            div className = {
+                'contactItem'
+            } >
+            <
+            img className = {
+                'contactImage'
+            }
+            src = {
+                './img/user.png'
+            }
+            />
 
-        <p className= {'contactLabel'}> Imię:  {this.props.item.firstName}</p>
-        <p className = {'contactLabel'}> Nazwisko:  {this.props.item.lastName}</p>
-        <a className= {'contactEmail'} href= {'mailto: ' + this.props.item.email}> {this.props.item.email}
-        </a>
-      </div>
-    )
-  },
+            <
+            p className = {
+                'contactLabel'
+            } > Imię: {
+                this.props.item.firstName
+            } < /p> <
+            p className = {
+                'contactLabel'
+            } > Nazwisko: {
+                this.props.item.lastName
+            } < /p> <
+            a className = {
+                'contactEmail'
+            }
+            href = {
+                'mailto: ' + this.props.item.email
+            } > {
+                this.props.item.email
+            } <
+            /a> <
+            /div>
+        )
+    },
 });
